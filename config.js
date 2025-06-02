4a7c91ddbee36401eee46af7aa2ffd6e651b47db
Jianan Lai <jlai@login04.af.uchicago.edu>
2025-06-02 11:20:53 -0500

    commit all

--- config.js
+++ config.js
@@ -14,6 +14,11 @@ const cut_config_dphi_diff = {
 const cut_config_sig = {
     'basic': true, 'selection': true
 }
+const cut_config_jets_faking_photons = {
+    'basic': true, 'BDTScore': true, 'dphi_met_phterm': true, 'metsig': true, 'ph_eta': true,
+    'dmet': true, 'dphi_met_jetterm': true, "dphi_ph_centraljet1": true
+}
+
 
 
 
@@ -31,8 +36,19 @@ const var_config_main = [
     "n_mu_baseline", "n_ph", "n_ph_baseline", "n_tau_baseline", "ph_eta", 
     "ph_phi", "ph_pt", "puWeight", "softerm", "trigger", "vtx_sumPt"
 ];
-
-
+const var_config_jets_faking_photons = [
+    "balance", "balance_sumet", "BDTScore",
+    "central_jets_fraction", "dmet", "dphi_jj", "dphi_met_central_jet", 
+    "dphi_met_jetterm", "dphi_met_ph", "dphi_met_phterm", "dphi_ph_jet1", 
+    "dphi_ph_centraljet1", "dphi_phterm_jetterm", "failJVT_jet_pt", 
+    "failJVT_jet_pt1", "jet_central_emfrac", "jet_central_eta", 
+    "jet_central_pt", "jet_central_pt1", "jet_central_pt2", "jet_central_timing", 
+    "jet_central_timing1", "jetterm", "jetterm_sumet", "met", "met_cst", 
+    "met_noJVT", "met_track", "metplusph", "metsig", "metsigres", 
+    "mt", "n_jet", "n_jet_central", "n_jet_fwd", "n_el_baseline", 
+    "n_mu_baseline", "n_ph", "n_ph_baseline", "n_tau_baseline", "ph_eta", 
+    "ph_phi", "ph_pt", "softerm", "trigger", "vtx_sumPt"
+];
 
 // Define the significance configurations
 const sig_config_main = [
@@ -45,7 +61,6 @@ const sig_config_dphi_diff = [
     "dphi_met_jetterm", "dphi_met_phterm", "dphi_ph_centraljet1", "dphi_phterm_jetterm", 
     "met", "metsig", "mt", "n_jet_central", "ph_eta", "ph_pt"
 ];
-
 
 
 // Define the n-1 configureations
@@ -95,5 +110,21 @@ const imageMap_index = {
         n_1_config: n_1_config_mt100,
         path: [``, `test/sigstudy/`],
         imagesPerRow: 4
-    }
+    },
+    "jets_faking_photons": {
+        cut_config: cut_config_jets_faking_photons,
+        var_config: var_config_jets_faking_photons,
+        sig_config: sig_config_main,
+        n_1_config: n_1_config_main,
+        path: [`jets_faking_photons/lumi26/`, `jets_faking_photons/lumi135/`],
+        imagesPerRow: 4
+    }, 
+    "electrons_faking_photons": {
+        cut_config: cut_config_jets_faking_photons,
+        var_config: var_config_jets_faking_photons,
+        sig_config: sig_config_main,
+        n_1_config: n_1_config_main,
+        path: [`electrons_faking_photons/lumi26/`, `electrons_faking_photons/lumi135/`],
+        imagesPerRow: 4
+    }, 
 };
