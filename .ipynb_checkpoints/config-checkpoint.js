d64cb7e4d535e6235b2d3d546934a355f8cc5b47
Jianan Lai <jlai@login01.af.uchicago.edu>
2025-11-06 00:01:44 -0600

    commit all

---
+++ .ipynb_checkpoints/config-checkpoint.js
@@ -0,0 +1,83 @@
+// Define the cut configurations (same as Python's cut_config)
+const cut_config_main = {
+    'basic': true, 'metsig': true, 'dphi_met_phterm': true, 'dmet': true,
+    'dphi_met_jetterm': true, 'ph_eta': true, 'dphi_jj': true, 'balance': true, 'mt2': true
+};
+const cut_config_jets_faking_photons3 = {
+    'basic': true
+}
+
+
+
+// Define the variable configurations
+const var_config_main = [
+    ['mt', 'balance', 'VertexBDTScore'],
+    ['n_ph', 'n_ph_baseline', 'n_el_baseline', 'n_mu_baseline', 'n_tau_baseline', 'pv_ntracks', 'n_jet', 
+     'n_jet_central', 'n_jet_fwd', 'central_jets_fraction'],
+    ['ph_pt', 'ph_eta', 'ph_phi'],
+    ['jet_central_eta', 'jet_central_pt1', 'jet_central_pt2', 'jet_central_vecSumPt', 'failJVT_jet_pt1'],
+    ['met', 'met_noJVT', 'metsig', 'metsigres', 'dmet', 'softerm', 'jetterm', 'jetterm_sumet'], 
+    ['dphi_met_phterm', 'dphi_met_jetterm', 'dphi_phterm_jetterm', 'dphi_jj']
+];
+     
+const var_config_jets_faking_photons3 = [
+    "balance", "balance_sumet", "VertexBDTScore",
+    "central_jets_fraction", "dmet", "dphi_jj", "dphi_met_central_jet", 
+    "dphi_met_jetterm", "dphi_met_ph", "dphi_met_phterm", "dphi_ph_jet1", 
+    "dphi_ph_centraljet1", "dphi_phterm_jetterm", "failJVT_jet_pt", 
+    "failJVT_jet_pt1", "jet_central_emfrac", "jet_central_eta", 
+    "jet_central_pt", "jet_central_pt1", "jet_central_pt2", "jet_central_timing", 
+    "jet_central_timing1", "jetterm", "jetterm_sumet", "met", "met_cst", 
+    "met_noJVT", "met_track", "metplusph", "metsig", "metsigres", 
+    "mt", "n_jet", "n_jet_central", "n_jet_fwd", "n_el_baseline", 
+    "n_mu_baseline", "n_ph", "n_ph_baseline", "n_tau_baseline", "ph_eta", 
+    "ph_phi", "ph_pt", "softerm", "trigger", "vtx_sumPt"
+];
+
+// Define the significance configurations
+const sig_config_main = [
+    "BDTScore", "balance", "dmet", "dphi_jj", "dphi_met_jetterm", 
+    "dphi_met_phterm", "dphi_ph_centraljet1", "dphi_phterm_jetterm", 
+    "met", "metsig", "mt", "n_jet_central", "ph_eta", "ph_pt"
+];
+
+// Define the n-1 configureations
+const n_1_config_main = [
+    "balance", "dmet", "dphi_jj", "dphi_met_jetterm", "dphi_met_phterm", 
+    "metsig", "mt", "ph_eta"
+];
+const n_1_config_jets_faking_photons = [
+    "BDTScore", "dphi_met_phterm", "metsig", "ph_eta", "dmet", "dphi_met_jetterm", "balance", "dphi_jj"];
+
+
+// Define group
+const group_titles_main = [
+  "Composite / Analysis Variables",
+  "Number of []",
+  "Photon / Electron Kinematics",
+  "Jet Kinematics",
+  "Missing ET",
+  "Angular Separations"
+];
+
+
+const imageMap_index = {
+    "main": {
+        cut_config: cut_config_jets_faking_photons3,
+        var_config: var_config_main,
+        sig_config: sig_config_main,
+        n_1_config: n_1_config_main,
+        path: [`main/lumi26/`, `main/lumi135/`],
+        imagesPerRow: 4
+    }, 
+    "jets_faking_photons3": {
+        cut_config: cut_config_jets_faking_photons3,
+        var_config: var_config_jets_faking_photons3,
+        sig_config: sig_config_main,
+        n_1_config: n_1_config_jets_faking_photons,
+        path: [`jets_faking_photons3/lumi26/`, `jets_faking_photons3/lumi135/`],
+        imagesPerRow: 4
+    },
+};
+
+imageMap_index.main.group_titles = group_titles_main;
---
+++ .ipynb_checkpoints/main_generate-checkpoint.js
@@ -0,0 +1,76 @@
+// different config.js based on the link
+let hash = window.location.hash.replace('#', '');
+console.log("Hash from URL: ", hash);
+if (!hash) {hash = "main"};
+
+// Get imageData dict
+const imageData = imageMap_index[hash] || { images: [], title: 'No plots found' };
+let imagesPerRow = imageData.imagesPerRow || 4; // Default to 4 if not set
+const cut_config = imageData.cut_config;
+const var_config = imageData.var_config;
+const sig_config = imageData.sig_config;
+const n_1_config = imageData.n_1_config;
+
+// Function to generate image paths dynamically
+function generateImagePaths(cut_name, mode, lumi) {
+    if (!cut_config[cut_name] && mode !== "n-1") return [];
+    
+    let images = [];
+    let path = (lumi === "26fb") ? imageData.path[0] : (lumi === "135fb") ? imageData.path[1] : ``;
+
+    if (mode == "performance") {
+        var_config.forEach((var_name) => {
+            images.push(path + `${cut_name}cut/${var_name}.png`);
+            images.push(path + `${cut_name}cut/roc_curve_${var_name}.png`);
+        })
+    } else if (mode == "significance") {
+        sig_config.forEach((sig_name) => {
+            images.push(path + `${cut_name}cut/${sig_name}.png`);
+            images.push(path + `${cut_name}cut/significance_${sig_name}_lowercut.png`);
+            images.push(path + `${cut_name}cut/significance_${sig_name}_uppercut.png`);
+        })
+    } else if (mode == "n-1") {
+        n_1_config.forEach((n_1_name) => {
+            images.push(path + `n-1cut/${n_1_name}.png`);
+            images.push(path + `n-1cut/significance_${n_1_name}_lowercut.png`);
+            images.push(path + `n-1cut/significance_${n_1_name}_uppercut.png`);
+        })
+    }
+
+    return images;
+}
+
+// Function to update images based on selected cut
+function updateImages(cut_name, mode, lumi) {
+    cutTitle.textContent = `mc23d + mc23e ${cut_name} cut ${mode} plots`;
+    imageContainer.innerHTML = "";
+
+    // Highlight the selected cut name
+    document.querySelectorAll("#cut-nav a").forEach(a => a.classList.remove("active"));
+    document.querySelectorAll(`a[data-cut='${cut_name}']`).forEach(a => a.classList.add("active"));
+
+    // Determine the number of images per row
+    let imagesPerRow = mode === "performance" ? 4 : 3;
+    imageContainer.style.display = "grid";
+    imageContainer.style.gridTemplateColumns = `repeat(${imagesPerRow}, 1fr)`;
+
+    const images = generateImagePaths(cut_name, mode, lumi);
+    images.forEach((img) => {
+        const container = document.createElement('div');
+        container.className = 'image-container';
+
+        const imgElement = document.createElement('img');
+        imgElement.src = img;
+        imgElement.alt = img.split('/').pop();
+        imgElement.onclick = () => openModal(img); // click to zoom
+
+        const filename = document.createElement('p');
+        filename.className = 'filename';
+        filename.textContent = img.split('/').pop();
+
+        container.appendChild(imgElement);
+        container.appendChild(filename);
+        imageContainer.appendChild(container);
+
+    })
+}
